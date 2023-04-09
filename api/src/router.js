ab7d944605700f3b58a2aa0cfca9c0f27390f708
nitesh-kumbalkar <nitesh.kumbalkar@gmail.com>
2023-04-09 12:52:28 +0530

    Update router.js for Exercise 5

--- api/src/router.js
+++ api/src/router.js
@@ -30,6 +30,9 @@ const upload = multer({
 router.post("/upload", upload.single("photo"), async (request, response) => {
   if (request.fileValidationError)
     return response.status(400).json({ error: request.fileValidationError });
+  try {
+    await imageProcessor(request.file.filename);
+  } catch (error) {}
   return response.status(201).json({ success: true });
 });
 
